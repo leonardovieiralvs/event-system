8840ac92cef2251881d681230c565e886083ad28
Leonardo <leonardo_vieira199@hotmail.com>
2026-04-14 07:33:16 -0300

    first commit

---
+++ src/test/java/com/lsouza/event_system/EventSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsouza.event_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
